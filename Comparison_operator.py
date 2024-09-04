31ccb25600534979622a64a7387c8c9a909495a8
YOGEESH K P <yogeesh793@gmail.com>
2024-09-04 14:35:09 +0530

    Add files via upload

---
+++ Comparison_operator.py
@@ -0,0 +1,15 @@
+# comparison operator
+a=5
+b=10
+# equal
+print("a==b:",a==b)
+# not equal
+print("a!=b:",a!=b)
+# greater than
+print("a>b:",a>b)
+# less than
+print("a<b:",a<b)
+# greater than or equal to
+print("a>=b:",a>=b)
+# less than or equal to
+print("a<=b:",a<=b)
